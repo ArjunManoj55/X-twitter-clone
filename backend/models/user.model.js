db2888abb59b3dda83937190eb54d455e82abe2c
Arjun Manoj <arjunmanoj5500@gmail.com>
2025-05-24 20:54:46 +0530

    db--schema/model-done

---
+++ backend/models/user.model.js
@@ -0,0 +1,64 @@
+import mongoose from "mongoose";
+
+//schema(structure)
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    followers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        default: [],
+      },
+    ],
+    following: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        default: [],
+      },
+    ],
+    profileImg: {
+      type: String,
+      default: "",
+    },
+    coverImg: {
+      type: String,
+      default: "",
+    },
+    bio: {
+      type: String,
+      default: "",
+    },
+    link: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
+
+//model
+const user = mongoose.model("User",userSchema);
+
+export default user;
+
+
